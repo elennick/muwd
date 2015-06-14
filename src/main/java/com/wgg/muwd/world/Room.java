0bf790f15892cf23addbc58baf8f75f3a8cd8db5
Evan Lennick <evan.lennick@accolade.com>
2015-06-14 11:55:55 -0400

    parsing directions

--- src/main/java/com/wgg/muwd/world/Room.java
+++ src/main/java/com/wgg/muwd/world/Room.java
@@ -1,10 +1,13 @@
 package com.wgg.muwd.world;
 
+import java.util.Map;
+
 public class Room {
 
     private Long id;
     private String name;
     private String description;
+    private Map<String, Long> directions;
 
     public Long getId() {
         return id;
@@ -30,12 +33,22 @@ public class Room {
         this.description = description;
     }
 
+    public Map<String, Long> getDirections() {
+        return directions;
+    }
+
+    public void setDirections(Map<String, Long> directions) {
+        this.directions = directions;
+    }
+
     @Override
     public String toString() {
         return "Room{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
+                ", directions=" + directions +
                 '}';
     }
+
 }
